5746ed3f74a1bb84ffaa8c36586535ad89cc3f02
Henrik Gullven <henrik.gullven@qamcom.se>
2019-01-29 19:51:45 +0100

    start

--- pusher.py
+++ pusher.py
@@ -10,13 +10,25 @@ from influxdb import InfluxDBClient
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
-# Connect to sheet
-credentials = ServiceAccountCredentials.from_json_keyfile_name(config.GOOGLE['credentials'], scope)
-gc = gspread.authorize(credentials)
-sh = gc.open_by_key(config.GOOGLE['key'])
-ws = sh.worksheet(config.GOOGLE['sheet'])
+class GooglePusher:
 
-# Collect data
+    def __init__ (self, data_collector_cb):
+        self.data_collector_cb = data_collector_cb
+        
+    def push(self)
+        # Connect to sheet
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(config.GOOGLE['credentials'], scope)
+        gc = gspread.authorize(credentials)
+        sh = gc.open_by_key(config.GOOGLE['key'])
+        ws = sh.worksheet(config.GOOGLE['sheet'])
+
+        # Collect data
+        newRow = self.data_collector_cb()
+        
+        # Write data to sheet
+        ws.append_row(newRow)
+
+
 
 #add time
 newRow = [str(time.strftime("%d/%m/%Y")), str(time.strftime("%H:%M:%S"))]
@@ -39,8 +51,3 @@ p = os.popen('cat /sys/class/thermal/thermal_zone0/temp', "r")
 RPiTemp = float(p.readline())
 RPiTemp = RPiTemp/1000.0
 newRow.extend([RPiTemp])
-
-# Write data to sheet
-ws.append_row(newRow)
-
-#print(newRow)
