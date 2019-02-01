7c4381247c9edb04818a633c9a311a96c061bff7
Henrik Gullven <henrik.gullven@qamcom.se>
2019-02-01 23:30:37 +0100

    logging works with google-auth

--- pusher.py
+++ pusher.py
@@ -3,6 +3,7 @@
 import config
 
 from google.oauth2 import service_account
+from google.auth.transport.requests import AuthorizedSession
 
 import gspread
 
@@ -18,22 +19,18 @@ class GooglePusher:
     def __init__ (self, config, data_collector_cb):
         self.data_collector_cb = data_collector_cb
         self.cfg = config
-        credentials = service_account.Credentials.from_service_account_file(cfg['credentials'])
-        self.scoped_credentials = credentials.with_scopes(cfg['scope'])
-        
-    def push(self):
-        # Collect data
-        newRow = self.data_collector_cb()
-        print("pushing: ")
-        print(newRow)
-        
-        
-    def push1(self):
-        # Connect to sheet
 
-        gc = gspread.authorize(self.scoped_credentials)
-        sh = gc.open_by_key(cfg['key'])
-        ws = sh.worksheet(cfg['sheet'])
+        self.credentials = service_account.Credentials.from_service_account_file(self.cfg['credentials'])
+        self.scoped_credentials = self.credentials.with_scopes(self.cfg['scope'])
+
+    def push(self):
+        
+        # Connect to sheet
+        gc = gspread.Client(auth=self.scoped_credentials)
+        gc.session = AuthorizedSession(self.scoped_credentials)
+        
+        sh = gc.open_by_key(self.cfg['key'])
+        ws = sh.worksheet(self.cfg['sheet'])
 
         # Collect data
         newRow = self.data_collector_cb()
@@ -47,30 +44,28 @@ class InfluxDbCollector:
     
     def __init__(self, config):
         self.cfg = config
-
-    def collect(self):
-        newRow = [str(time.strftime("%d/%m/%Y")), str(time.strftime("%H:%M:%S"))]
-
-        return newRow
     
-    def collect1(self):
+    def collect(self):
     
         #add time
         newRow = [str(time.strftime("%d/%m/%Y")), str(time.strftime("%H:%M:%S"))]
 
         # Instantiate a connection to the InfluxDB
-        client = InfluxDBClient(cfg['host'], cfg['port'], cfg['user'], cfg['password'], cfg['dbname'])
+        client = InfluxDBClient(self.cfg['host'], self.cfg['port'], self.cfg['user'], self.cfg['password'], self.cfg['dbname'])
         
         # Get sensor data
-        res = client.query(cfg['query'])
-
+        res = client.query(self.cfg['query'])
+        
         # iterate the sensors
         for sensor in res:
-            t  = sensor[0]['temperature']
-            h  = sensor[0]['humidity']
-            ah = sensor[0]['abs_humidity']
-            d  = sensor[0]['dewpoint']
-            newRow.extend([t, h, ah, d])
+            t = sensor[0]['time']
+            val  = sensor[0]['mean']
+            newRow.extend([t, val])
+            #t  = sensor[0]['temperature']
+            #h  = sensor[0]['humidity']
+            #ah = sensor[0]['abs_humidity']
+            #d  = sensor[0]['dewpoint']
+            #newRow.extend([t, h, ah, d])
             
         return newRow 
 
