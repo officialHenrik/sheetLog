f259edba87cabd3f72f94dda35b6b65461adcbd7
Henrik Gullven <henrik.gullven@qamcom.se>
2019-02-01 23:38:02 +0100

    store session and client

--- pusher.py
+++ pusher.py
@@ -22,21 +22,21 @@ class GooglePusher:
 
         self.credentials = service_account.Credentials.from_service_account_file(self.cfg['credentials'])
         self.scoped_credentials = self.credentials.with_scopes(self.cfg['scope'])
+        
+        # Connect to sheet
+        self.gc = gspread.Client(auth=self.scoped_credentials)
+        self.gc.session = AuthorizedSession(self.scoped_credentials)
+        
+        self.sh = self.gc.open_by_key(self.cfg['key'])
+        self.ws = self.sh.worksheet(self.cfg['sheet'])
 
     def push(self):
         
-        # Connect to sheet
-        gc = gspread.Client(auth=self.scoped_credentials)
-        gc.session = AuthorizedSession(self.scoped_credentials)
-        
-        sh = gc.open_by_key(self.cfg['key'])
-        ws = sh.worksheet(self.cfg['sheet'])
-
         # Collect data
         newRow = self.data_collector_cb()
         
-        # Write data to sheet
-        ws.append_row(newRow)
+        # Write data to end of sheet
+        self.ws.append_row(newRow)
 
 
 # ------------------------------------------------------
@@ -44,28 +44,20 @@ class InfluxDbCollector:
     
     def __init__(self, config):
         self.cfg = config
+        # Instantiate a connection to the InfluxDB
+        self.client = InfluxDBClient(self.cfg['host'], self.cfg['port'], self.cfg['user'], self.cfg['password'], self.cfg['dbname'])
     
     def collect(self):
     
+        # Get sensor data
+        res = self.client.query(self.cfg['query'])
+        
         #add time
         newRow = [str(time.strftime("%d/%m/%Y")), str(time.strftime("%H:%M:%S"))]
-
-        # Instantiate a connection to the InfluxDB
-        client = InfluxDBClient(self.cfg['host'], self.cfg['port'], self.cfg['user'], self.cfg['password'], self.cfg['dbname'])
-        
-        # Get sensor data
-        res = client.query(self.cfg['query'])
-        
         # iterate the sensors
         for sensor in res:
-            t = sensor[0]['time']
-            val  = sensor[0]['mean']
-            newRow.extend([t, val])
-            #t  = sensor[0]['temperature']
-            #h  = sensor[0]['humidity']
-            #ah = sensor[0]['abs_humidity']
-            #d  = sensor[0]['dewpoint']
-            #newRow.extend([t, h, ah, d])
+            for key in sensor[0]:
+                newRow.extend([sensor[0][key]])
             
         return newRow 
 
