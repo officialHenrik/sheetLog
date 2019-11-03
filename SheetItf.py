4d0bacf762606cf4a9d0cb1fcd443f926dc65fcc
HgPi4 <hg@example.se>
2019-11-03 14:53:29 +0100

    do not use config.py

--- SheetItf.py
+++ SheetItf.py
@@ -7,18 +7,18 @@ import gspread
 # ------------------------------------------------------
 class SheetItf:
 
-    def __init__ (self, config):
+    def __init__ (self, credentials, scope, key, sheet):
         self.cfg = config
 
-        self.credentials = service_account.Credentials.from_service_account_file(self.cfg['credentials'])
-        self.scoped_credentials = self.credentials.with_scopes(self.cfg['scope'])
+        self.credentials = service_account.Credentials.from_service_account_file(credentials)
+        self.scoped_credentials = self.credentials.with_scopes(scope)
         
         # Connect to sheet
         self.gc = gspread.Client(auth=self.scoped_credentials)
         self.gc.session = AuthorizedSession(self.scoped_credentials)
         
-        self.sh = self.gc.open_by_key(self.cfg['key'])
-        self.ws = self.sh.worksheet(self.cfg['sheet'])
+        self.sh = self.gc.open_by_key(key)
+        self.ws = self.sh.worksheet(sheet)
         
         self.nextRow = []
 
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     import config
     import time
     
-    gp = SheetItf(config.GOOGLE)
+    gp = SheetItf(self.cfg['credentials'], self.cfg['scope'], self.cfg['key'], self.cfg['sheet'])
     gp.addToRow([str(time.strftime("%d/%m/%Y")), str(time.strftime("%H:%M:%S"))])
     gp.addToRow(["row 1"])
     gp.pushRow()
